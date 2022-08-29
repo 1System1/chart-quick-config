1f8615023f9695f81a6f3c3d96612db4aae26a27
1System1 <42433115+1System1@users.noreply.github.com>
2022-08-29 10:31:25 +0800

    Add files via upload
    第一次提交

---
+++ src/views/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div class="home-head">
+      <!-- 头部显示所有图形类型 -->
+      <!-- 选中蓝色，未选中灰色 -->
+      <el-tag type="info">标签三</el-tag>
+    </div>
+    <div class="all-chart">
+      <!-- 显示所有图形配置，卡片-懒加载形式展示 -->
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+// 首页显示所有图形
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
---
+++ src/views/keys/Chart.js
@@ -0,0 +1,14 @@
+// 静态常量文件
+export default class ChartKey{
+    // 图形类型
+    static CHART_TYPES=[
+                    {
+                        type:"bar",
+                        text:"柱状图",
+                    },
+                    {
+                        type:"line",
+                        text:"折线图",
+                    }
+    ]
+}
